1d6114f73b2ebcd50992df4f7430bdea2d2dd8c4
jojoX94 <jose.augusteramarolahy@gmail.com>
2023-08-21 10:14:34 +0300

    add custom fonts + enable eslint and prettier formatter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,29 @@
 module.exports = {
+  env: {
+    'jest/globals': true,
+  },
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native-community', 'prettier'],
+  rules: {
+    'react-hooks/exhaustive-deps': 'off',
+    'object-curly-spacing': ['off', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+    'react/require-default-props': ['error'],
+    'react/default-props-match-prop-types': ['error'],
+    'react/sort-prop-types': ['error'],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react-native/no-inline-styles': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
 };
